98d3712f8ea98b75ca1e9b9b7f4aee0c752a2852
Pelnatt <hunlove65@gmail.com>
2025-04-02 11:46:29 +0900

    MainMenuGameMode 작업중

--- Source/CS/Private/GameModes/CSMainMenuGameMode.cpp
+++ Source/CS/Private/GameModes/CSMainMenuGameMode.cpp
@@ -1,2 +1,34 @@
 #include "GameModes/CSMainMenuGameMode.h"
+#include "Kismet/GameplayStatics.h"
 
+ACSMainMenuGameMode::ACSMainMenuGameMode()
+{
+	MatchType = EMainMenuMatchType::SinglePlayer;
+}
+
+void ACSMainMenuGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 메인 메뉴 초기화 로직을 여기에 추가할 수 있습니다.
+}
+
+void ACSMainMenuGameMode::StartSinglePlayer()
+{
+	UGameplayStatics::OpenLevel(this, FName("TutorialMap"));
+}
+
+void ACSMainMenuGameMode::StartMultiplayer()
+{
+	// 멀티플레이어 모드를 시작하는 로직을 여기에 추가합니다.
+	// 예: 멀티플레이어 로비로 이동
+}
+
+void ACSMainMenuGameMode::QuitGame()
+{
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+	if (PlayerController)
+	{
+		PlayerController->ConsoleCommand("quit");
+	}
+}
--- Source/CS/Public/GameModes/CSMainMenuGameMode.h
+++ Source/CS/Public/GameModes/CSMainMenuGameMode.h
@@ -1,12 +1,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameMode.h"
+#include "GameFramework/GameModeBase.h"
+#include "CSTypes/CSGameTypes.h"
 #include "CSMainMenuGameMode.generated.h"
 
+UENUM(BlueprintType)
+enum class EMainMenuMatchType : uint8
+{
+	SinglePlayer UMETA(DisplayName = "SinglePlayer"),
+	Multiplayer UMETA(DisplayName = "Multiplayer")
+};
+
 UCLASS()
-class CS_API ACSMainMenuGameMode : public AGameMode
+class CS_API ACSMainMenuGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	ACSMainMenuGameMode();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "GameMode")
+	void StartSinglePlayer();
+
+	UFUNCTION(BlueprintCallable, Category = "GameMode")
+	void StartMultiplayer();
+
+	UFUNCTION(BlueprintCallable, Category = "GameMode")
+	void QuitGame();
+
+private:
+	EMainMenuMatchType MatchType;
 };
