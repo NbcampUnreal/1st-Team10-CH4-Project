7e806c564562c0dcf7b504dbe509bd0136d7e4e8
Dongry-96 <kevin92854654321@gmail.com>
2025-04-03 17:09:43 +0900

    [Add] 캐릭터 데이터 테이블 추가

---
+++ Source/CS/Public/Data/CSCharacterRow.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "CSCharacterRow.generated.h"
+
+USTRUCT(BlueprintType)
+struct FCharacterRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FName CharacterID;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TSoftClassPtr<APawn> CharacterClass;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TSoftObjectPtr<UTexture2D> Portrait;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FText DisplayName;
+};
