5a35cb81f6a9f426868ad15da6e7a8750beaaf90
kimjy0730 <kimjy2767@naver.com>
2025-04-04 19:26:06 +0900

    [Add] 체이싱 플레이어 테스크

--- Source/CS/Public/AI/BT/BTTask_ChasePlayers.h
+++ Source/CS/Public/AI/BT/BTTask_ChasePlayers.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,9 +5,7 @@
 #include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
 #include "BTTask_ChasePlayers.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class CS_API UBTTask_ChasePlayers : public UBTTask_BlackboardBase
 {
