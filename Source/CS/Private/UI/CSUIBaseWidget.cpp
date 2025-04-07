ee1e0bee3d2a9f73c5aaf3db7bf623256441adfe
emongscs <you6598@naver.com>
2025-04-07 20:43:12 +0900

    [Update] UI 부모클래스에 자식클래스들이 사용할 함수들 선언 및 MainMenuWidget 바꿈

--- Source/CS/Private/UI/CSUIBaseWidget.cpp
+++ Source/CS/Private/UI/CSUIBaseWidget.cpp
@@ -3,13 +3,37 @@
 
 #include "UI/CSUIBaseWidget.h"
 
-void UCSUIBaseWidget::NativeConstruct()
+void UCSUIBaseWidget::UpdateReadyUI_Implementation(bool bIsReady)
 {
-    Super::NativeConstruct();
-
+    // 부모에서는 아무런 로직도 구현하지 않음.
 }
 
-void UCSUIBaseWidget::UpdateUI()
+void UCSUIBaseWidget::UpdateTeamUI_Implementation(int32 TeamID)
 {
+    // 부모에서는 아무런 로직도 구현하지 않음.
+}
 
+void UCSUIBaseWidget::UpdateCharacterUI_Implementation(FName SelectedCharacterID)
+{
+    // 부모에서는 아무런 로직도 구현하지 않음.
+}
+
+void UCSUIBaseWidget::UpdateSelectedMapUI_Implementation(FName SelectedMap)
+{
+    // 부모에서는 아무런 로직도 구현하지 않음.
+}
+
+void UCSUIBaseWidget::UpdateMatchTimeUI_Implementation(int32 Time)
+{
+    // 부모에서는 아무런 로직도 구현하지 않음.
+}
+
+void UCSUIBaseWidget::OnMatchPhaseChanged_Implementation()
+{
+    // 부모에서는 아무런 로직도 구현하지 않음.
+}
+
+void UCSUIBaseWidget::OnSuddenDeathUI_Implementation()
+{
+    // 부모에서는 아무런 로직도 구현하지 않음.
 }
