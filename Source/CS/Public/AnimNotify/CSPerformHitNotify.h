8476ed7c76e7c20d5395f86a72d325e76b4e80c7
kimjy0730 <kimjy2767@naver.com>
2025-04-07 14:34:53 +0900

    [Update] AI 공격 몽타주 설정 [Add]히트 노티파이설정

---
+++ Source/CS/Public/AnimNotify/CSPerformHitNotify.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "CSPerformHitNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CS_API UCSPerformHitNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+};
